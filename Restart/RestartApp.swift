823d1f3c1a28bc4cf53eea86366cc4457613c8af
ridhomujizat <ridhomujizat@gmail.com>
2022-04-28 21:20:18 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by ridho mujizat on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
